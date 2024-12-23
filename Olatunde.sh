29922e847617345bb50fca732537ac9e5e3cee7c
olatunde <horlartundhey@gmail.com>
2024-12-23 13:37:35 +0100

    My commit to the bash scripting assignment

---
+++ Olatunde.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+who_am_i(){
+    echo "Hello, I am Ibitoye Olatunde"
+}
+
+Bio(){
+    echo "I am in the Cloud Engineering track with student Id: 
+    ALT/SOE/024/1312"
+    echo "I am 29 years old, I am currently a freelancer web developer, working with
+    various clients to build solutions and deploy websites and web application"
+    echo "I love reading mangas, watching anime and playing games"
+}
+
+Years_in_tech(){
+    echo "I've been in tech for close to 6 years"
+}
+
+Dream_job(){
+    echo "I haven't thought this far, I just know I want to use this 
+    skill to optimize my development skills, adding Devops to my Java Skills
+    will give me the right opportunity I need, to scale up "
+}
+
+Goals(){
+    echo "A professional in devops, and application deployments"
+    echo "To apply to high end jobs that pay well "
+    echo "Use this skill to build products I can fully utilize and even sell"
+    echo "Create a simple cloud community that will foster growth across board for everyone, from certificate to next steps"    
+}
+
+who_am_i
+echo
+
+Bio
+echo
+
+Years_in_tech
+echo
+
+Dream_job
+echo
+
+Goals
+echo
